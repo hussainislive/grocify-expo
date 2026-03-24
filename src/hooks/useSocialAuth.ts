6f0936e41187d5d869c57a696710aeebae08cc9d
hussainislive <developer.hussain125@gmail.com>
2026-03-24 22:31:06 +0500

    feat: implement social authentication with Google, GitHub, and Apple; add themed layout and update dependencies

---
+++ src/hooks/useSocialAuth.ts
@@ -0,0 +1,40 @@
+import { useSSO } from "@clerk/expo";
+import { useState } from "react";
+import { Alert } from "react-native";
+
+const useSocialAuth = () => {
+  const [loadingStrategy, setLoadingStrategy] = useState<string | null>(null);
+  const { startSSOFlow } = useSSO();
+
+  const handleSocialAuth = async (
+    strategy: "oauth_google" | "oauth_github" | "oauth_apple",
+  ) => {
+    if(loadingStrategy) return;
+    setLoadingStrategy(strategy);
+
+    try {
+      const { createdSessionId, setActive } = await startSSOFlow({ strategy });
+
+      if (!createdSessionId || !setActive) {
+        Alert.alert(
+          "Sign-in incomplete",
+          "Failed to complete social authentication. Please try again.",
+        );
+        return;
+      }
+
+      await setActive({ session: createdSessionId });
+    } catch (error) {
+      console.log("Error is social auth:", error);
+      Alert.alert(
+        "Error",
+        "An error occurred during social authentication. Please try again.",
+      );
+    } finally {
+      setLoadingStrategy(null);
+    }
+  };
+  return { handleSocialAuth, loadingStrategy };
+};
+
+export default useSocialAuth;
