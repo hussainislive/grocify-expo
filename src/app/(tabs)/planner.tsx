02f50c17ff427a7bbb430882c3f4a1a7220f298a
hussainislive <developer.hussain125@gmail.com>
2026-03-29 16:32:48 +0500

    feat: add tabs layout and screens for insights and planner

---
+++ src/app/(tabs)/planner.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const PlannerScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-white">PlannerScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default PlannerScreen;
